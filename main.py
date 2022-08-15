0bd21b4efd02560c1633a37131f4e146d5bdb5c1
WorkStation <codewithafrica@gmail.com>
2022-08-15 13:19:20 +0300

    add propagate,optimize and predict function

--- main.py
+++ main.py
@@ -17,10 +17,35 @@ print(Y_train.shape)
 print(X_test.shape)
 print(Y_test.shape)
 
+num_iterations = 2000
+learning_rate = 0.005
+print_cost = True
+
 dims = X_train.shape[0]
 w, b = initialize_parameters(dims)
+params, grads, costs = optimize(w, b, X_train, Y_train, learning_rate, num_iterations, print_cost)
 
-print(w.shape)
+w = params["w"]
+b = params["b"]
+
+Y_prediction_test = predict(w, b, X_test)
+Y_prediction_train = predict(w, b, X_train)
+
+if print_cost:
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+results = {
+    "costs": costs,
+    "Y_prediction_test": Y_prediction_test,
+    "Y_prediction_train": Y_prediction_train,
+    "w": w,
+    "b": b,
+    "learning_rate": learning_rate,
+    "num_iterations": num_iterations
+}
+
+
 
 
 
--- packages/modules.py
+++ packages/modules.py
@@ -1,3 +1,4 @@
+import copy
 from turtle import shape
 import pandas as pd
 import numpy as np
@@ -39,16 +40,67 @@ def initialize_parameters(dims):
 
     return w, b
 
-def propagate(W, b, X_train, Y_train):
+def propagate(w, b, X_train, Y_train):
 
     # number of training examples
     m = X_train.shape[1]
     
     # activation function
-    A = sigmoid(np.dot(W.T, X_train) + b)
-    
+    A = sigmoid(np.dot(w.T, X_train)+ b)
+    print(A)
     # cost function
     cost  = -1/m *np.sum(np.dot(Y_train, np.log(A.T) + np.dot(1-Y_train, np.log(1 - A.T))))
+    dw = (1/m)* np.dot(X_train, (A-Y_train).T)
+    db = (1/m) * np.sum(A-Y_train)
+    
+    cost = np.squeeze(np.array(cost))
+    grads = {
+        "dw": dw,
+        "db": db
+    }
 
-    dw = (1/m)*
-    db = 
+    return grads, cost
+
+def optimize(w, b, X_train, Y_train, learning_rate =0.009 , num_iterations=100, print_cost=False):
+    w = copy.deepcopy(w)
+    b = copy.deepcopy(b)
+
+    costs = []
+
+    for i in range(num_iterations):
+        grads, cost = propagate(w, b, X_train, Y_train)
+
+        dw = grads["dw"]
+        db = grads["db"]
+
+        w = w - learning_rate*dw
+        b = b - learning_rate*db
+
+
+        if i % 100 == 0:
+            costs.append(cost)
+            print(f"Cost after iteration {i}: {cost}.")
+    
+    params = {"w": w, "b": b}
+
+    grads = {"dw": dw, "db": db}
+
+    return params, grads, costs
+
+def predict(w, b, X_train):
+    
+    m = X_train.shape[1]
+    Y_prediction = np.zeros((1, m))
+    w = w.reshape(X_train.shape[0], 1)
+    print(w.shape)
+
+    A = sigmoid(np.dot(w.T, X_train) + b)
+    for i in range(A.shape[1]):
+        if A[0, i] > 0.5:
+            Y_prediction[0,i] = 1
+        else:
+            Y_prediction[0, i] = 0
+
+    return Y_prediction
+
+
